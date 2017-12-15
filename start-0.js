3151420d2e9318978c2dfe0d08004d54d40faa4a
stavvie34 <alexanderlee58col@gmail.com>
2017-12-15 12:42:23 -0800

    changes

--- start-0.js
+++ start-0.js
@@ -1,8 +1,10 @@
 hosts = ['foodnstuff', 'sigma-cosmetics', 'joesguns', 'nectar-net', 'hong-fang-tea', 'harakiri-sushi', 'neo-net', 'CSEC', 'zer0', 'max-hardware', 'iron-gym', 'phantasy', 'silver-helix', 'omega-net', 'avmnite-02h', 'crush-fitness', 'johnson-ortho', 'the-hub'];
+
 while(true) {
     for (i = 0; i < hosts.length; i = i + 1) {
-        host = hosts[i];
-        print(host);
+		host = hosts[i];
+		vServerRam = getServerRam(host)
+        tprint(host);
         if (hasRootAccess(host) == false) {
             if (fileExists('brutessh.exe') == true)
                 brutessh(host);
@@ -15,20 +17,27 @@ while(true) {
             if (fileExists('sqlinject.exe') == true)
                 sqlinject(host);
             nuke(host);
-            print('nuked');
-			scp('hack-iron-gym.script', (host));
-			print('"Hack Iron-Gym" Script copied')
+		};
+        if (hasRootAccess(host) == true) {
+			tprint('Root Access Gained');
+		};
+		scp('hack-iron-gym.script', (host));
+		tprint('"Hack Iron-Gym" Script copied');
         };
-        if (getHackingLevel() >= getServerRequiredHackingLevel(host)) {         
-            if (getServerRam(host) == 8.00) {
+		tprint(getServerRam(host));
+        if (getHackingLevel() >= getServerRequiredHackingLevel(host)) {
+            if (vServerRam[0] == 8.00) {
 				exec('hack-iron-gym.script', host, 3, 'iron-gym');
-			} else if (getServerRam(host) == 16.00) {
+				tprint('Threads run for 8GB server');
+			} else if (vServerRam[0] == 16.00) {
 				exec('hack-iron-gym.script', host, 6, 'iron-gym');
-				} else if (getServerRam(host) == 32.00) {
+				tprint('Threads run for 16GB server');
+				} else if (vServerRam[0] == 32.00) {
 						exec('hack-iron-gym.script', host, 12, 'iron-gym');
-					} else if (getServerRam(host) == 64.00) {
+						tprint('Threads run for 32GB server');
+					} else if (vServerRam[0] == 64.00) {
 							exec('hack-iron-gym.script', host, 24, 'iron-gym');
+							tprint('Threads run for 64GB server');
 						};
 		};
     };
-};
