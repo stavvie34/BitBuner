c16069a1bef0e5e7fcb0fb93aa3ebc92b0919dec
stavvie34 <alexanderlee58col@gmail.com>
2017-12-19 10:46:36 -0800

    change

--- find-best.js
+++ find-best.js
@@ -1,15 +1,23 @@
 hosts = ['foodnstuff', 'sigma-cosmetics', 'joesguns', 'nectar-net', 'hong-fang-tea', 'harakiri-sushi', 'neo-net', 'CSEC', 'zer0', 'max-hardware', 'iron-gym', 'phantasy', 'silver-helix', 'omega-net', 'avmnite-02h', 'crush-fitness', 'johnson-ortho', 'the-hub'];
 
+
 for (i = 0; i < hosts.length; i = i + 1) {
     host = hosts[i];
-    serverRam = getServerRam(host);
+    svrWeakenTime = getWeakenTime(host);
+    svrHackTime = getHackTime(host);
+    svrGrowTime = getGrowTime(host);
+    svrMinScrLvl = getServerMinSecurityLevel(host);
+    svrMaxCash = getServerMaxMoney(host);
+	svrGrowth = getServerGrowth(host);
     
     tprint('There is ' + serverRam[0] + 'GB of RAM on ' + host);
 }
 
-for (j = 0; j < hosts.length; j = j + 1) {
-    host = hosts[j];
-    server = getServerRam(host);
-    
-    tprint('There is ' + serverRam[0] + 'GB of RAM on ' + host);
-}
+
+best = 
+lowest weaken time
+lowest hack time
+lowest grow time
+lowest minsecuritylevel
+greatest amount of max cash
+greatest growth parameter
