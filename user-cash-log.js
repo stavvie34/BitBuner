a983f171e86befe48e5f84edb9815f42877b14fe
stavvie34 <alexanderlee58col@gmail.com>
2017-12-20 20:44:57 -0800

    Working and the logic added makes sense

--- user-cash-log.js
+++ user-cash-log.js
@@ -13,8 +13,14 @@ for (i =0; i < 1000; i = i + 1) {
 	tprint('------------------------------');
 	tprint('You have $' + wallet[i]);
 	tprint('------------------------------');
-
-	tprint('You have gained $' + (wallet[i] - wallet[i - 1]) + ' in the last 10 seconds.');
+	
+	if ((wallet[i] - wallet[i - 1]) > 0) {
+		tprint('You have gained $' + (wallet[i] - wallet[i - 1]) + ' in the last 10 seconds.');
+	} else if ((wallet[i] - wallet[i - 1]) < 0) {
+		tprint('You have lost $' + (wallet[i] - wallet[i - 1]) + ' in the last 10 seconds.');
+	} else {
+		tprint('No growth or loss has occured.');
+	}
 	
 	sleep(10000);
 }
