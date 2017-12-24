a9e3a622fa4d9f84e179d6fe4f340861d377588b
Alex <stavvie34@users.noreply.github.com>
2017-12-23 23:35:35 -0800

    dont need em

--- Scripts to Keep/buy-servers.js
+++
@@ -1,9 +0,0 @@
-i = 0;
-while(i < 25) {
-    if (getServerMoneyAvailable("home") > 51200000) {
-        purchaseServer("server-" + i, 1024);
-        
-        //exec("early-hack-template.script", hostname, 1, "joesguns", 1000000000, 10);
-        ++i;
-    }
-}
