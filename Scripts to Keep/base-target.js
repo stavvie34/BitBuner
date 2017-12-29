417c55c6fd2c93b38f00a9d0544a06dd84b41892
stavvie34 <alexanderlee58col@gmail.com>
2017-12-28 16:26:10 -0800

    change loop

--- Scripts to Keep/base-target.js
+++ Scripts to Keep/base-target.js
@@ -1,6 +1,6 @@
 target = args[0];
 
-moneyThresh = getServerMaxMoney(target) * 0.9;
+moneyThresh = getServerMaxMoney(target) * 0.30;
 securityThresh = round(getServerBaseSecurityLevel(target) / 3) + 2;
 
 
@@ -20,21 +20,21 @@ if (hasRootAccess(target) === false) {
     
 }
 
-while(true) {
-    if (getServerSecurityLevel(target) > securityThresh) {
-        //If the server's security level is above our threshold, weaken it
-        weaken(target);
-    } else 
-    
-    svrSecurityLvl = getServerSecurityLevel(target);
-    svrBaseSecurityLvl = getServerBaseSecurityLevel(target);
-    
-    
-    if (getServerMoneyAvailable(target) < getServerMaxMoney(target)) {
-        //If the server's money is less than our threshold, grow it
-        grow(target);
-    } else {
-        //Otherwise, hack it
-        hack(target);
-    }
+while (true) {
+	if (getServerSecurityLevel(target) > securityThresh) {
+		//If the server's security level is above our threshold, weaken it
+		weaken(target);
+	} 	else {
+		svrSecurityLvl = getServerSecurityLevel(target);
+		svrBaseSecurityLvl = getServerBaseSecurityLevel(target);
+
+
+		if (getServerMoneyAvailable(target) < moneyThresh) {
+			//If the server's money is less than our threshold, grow it
+			grow(target);
+		} else {
+    		//Otherwise, hack it
+    		hack(target);
+		}
+	}
 }
