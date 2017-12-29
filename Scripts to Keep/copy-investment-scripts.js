28eb641c13c1dad1a332a3c7ab1aa6a716afc89d
stavvie34 <alexanderlee58col@gmail.com>
2017-12-28 16:25:43 -0800

    dont nede

--- Scripts to Keep/copy-investment-scripts.js
+++
@@ -1,6 +0,0 @@
-// COPY Investment Scripts to server-10
-
-scp('InvestmentBroker.script', 'server-10');
-scp('_InvestmentBroker.script', 'server-10');
-scp('_InvestmentBroker_Data.script', 'server-10');
-scp('_InvestmentBroker_Manager.script', 'server-10');
