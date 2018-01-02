ecaf53bb0dcacb6d12036e2b8ec168371b836c8c
stavvie34 <alexanderlee58col@gmail.com>
2018-01-02 11:50:09 -0800

    New stuff for the earlygame

--- Scripts to Keep/main-start-earlygame.js
+++ Scripts to Keep/main-start-earlygame.js
@@ -1,10 +1,10 @@
 //Servers with 0 port
-serverZeroPort = ['foodnstuff', 'sigma-cosmetics', 'joesguns', 'hong-fang-tea', 'harakiri-sushi'];
+//serverZeroPort = ['foodnstuff', 'sigma-cosmetics', 'joesguns', 'hong-fang-tea', 'harakiri-sushi'];
 
 target = args[0];
 
 //Servers with 1 port.
-//serverOnePort = ['foodnstuff', 'sigma-cosmetics', 'joesguns', 'nectar-net', 'hong-fang-tea', 'harakiri-sushi', 'neo-net', 'zer0', 'max-hardware', 'iron-gym'];
+serverOnePort = ['foodnstuff', 'sigma-cosmetics', 'joesguns', 'nectar-net', 'hong-fang-tea', 'harakiri-sushi', 'neo-net', 'zer0', 'max-hardware', 'iron-gym'];
 
 //All servers that aren't faction servers.
 //scanArray = ['foodnstuff', 'sigma-cosmetics', 'joesguns', 'nectar-net', 'hong-fang-tea', 'harakiri-sushi', 'neo-net', 'zer0', 'max-hardware', 'iron-gym', 'phantasy', 'silver-helix', 'omega-net', 'crush-fitness', 'johnson-ortho', 'the-hub', 'comptek', 'netlink', 'rothman-uni', 'catalyst', 'summit-uni', 'rho-construction', 'millenium-fitness', 'aevum-police', 'alpha-ent', 'syscore', 'lexo-corp', 'snap-fitness', 'global-pharm', 'applied-energetics', 'unitalife', 'nova-med', 'zb-def', 'zb-institute', 'vitalife', 'titan-labs', 'solaris', 'microdyne', 'helios', 'deltaone', 'icarus', 'omnia', 'defcomm', 'galactic-cyber', 'infocomm', 'taiyang-digital', 'stormtech', 'aerocorp', 'clarkeinc', 'omnitek', '4sigma', 'blade', 'b-and-a', 'ecorp', 'fulcrumtech', 'megacorp', 'kuai-gong', 'fulcrumassets', 'powerhouse-fitness'];
@@ -18,12 +18,60 @@ tprint('////////////////////////////////////////////////');
 tprint('     Running script on Zero Port Servers...     ');
 tprint('////////////////////////////////////////////////');
 
-for (i = 0; i < serverZeroPort.length; i = i + 1) {
-	currentServer = serverZeroPort[i];
-	usrHackLvl = getHackingLevel();
-	reqHackLvl = getServerRequiredHackingLevel(currentServer);
-	serverRamAvailable = ((getServerRam(currentServer)[0]) - (getServerRam(currentServer)[1]));
-	scriptRam = getScriptRam('base-target.script', 'home');
+homeRamAvailable = ((getServerRam('home')[0]) - (getServerRam('home')[1]));
+homeScriptRam = getScriptRam('base-target.script', 'home');
+homeThreads = (Math.floor((homeRamAvailable / homeScriptRam)));
+
+exec('base-target.script', 'home', homeThreads, target);
+
+for (i = 0; i < serverOnePort.length; i = i + 1) {
+	currentServer = serverOnePort[i];
+	
+	if ((isRunning('base-target.script', currentServer)) === false) {
+		while (getServerRequiredHackingLevel(currentServer) > getHackingLevel()) {
+			sleep(10000);
+		}
+
+		serverRamAvailable = ((getServerRam(currentServer)[0]) - (getServerRam(currentServer)[1]));
+		scriptRam = getScriptRam('base-target.script', 'home');
+
+		if (serverRamAvailable >= scriptRam) {
+			maxThreads = (Math.floor((serverRamAvailable) / (scriptRam)));
+
+			scp('base-target.script', currentServer);
+
+			if (hasRootAccess(currentServer) === false) {
+				if (fileExists('brutessh.exe', 'home') === true)
+					brutessh(currentServer);
+				if (fileExists('ftpcrack.exe') === true)
+					ftpcrack(currentServer);
+				if (fileExists('relaysmtp.exe') === true)
+					relaysmtp(currentServer);
+				if (fileExists('httpwork.exe') === true)
+					httpworm(currentServer);
+				if (fileExists('sqlinject.exe') === true)
+					sqlinject(currentServer);
+				nuke(currentServer);
+			}
+
+			exec('base-target.script', currentServer, maxThreads, target);
+			print('Script is running on ' + currentServer + '.');
+		} else {
+			print('There was an error or there was not enough ram on the server');
+		}
+	} else {
+		print('Script is already running on ' + currentServer + '.');
+	}
+	
+	
+	
+}
+
+/*
+	
+	
+	
+	
 	
 	if ((scriptRunning('base-target.script', currentServer)) === true) {
 		print('Script already running on ' + currentServer + '.');		
@@ -42,9 +90,9 @@ for (i = 0; i < serverZeroPort.length; i = i + 1) {
 }
 
 tprint(' ');
-tprint('///////////////////////////////////////////////');
-tprint('  Script is running on all Zero Port Servers.  ');
-tprint('///////////////////////////////////////////////');
+tprint('/////////////////////////////////////');
+tprint('  Script is running on all Servers.  ');
+tprint('/////////////////////////////////////');
 
 tprint(' ');
 tprint('////////////////////////////');
@@ -75,11 +123,12 @@ if ((scriptRunning('base-target.script', 'home')) === true) {
 	tprint('     Not enough RAM available on home.     ');
 	tprint('///////////////////////////////////////////');
 }
-*/	
+	
 	maxThreads = Math.floor((serverRamAvailable - getScriptRam('main-start-earlygame.script', 'home')) / scriptRam);
 	exec('base-target.script', 'home', maxThreads, target);
 }
 
+*/
 
 
 tprint(' ');
